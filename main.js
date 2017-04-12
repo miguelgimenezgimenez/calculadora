17f1c7d90bd04aa82aeb4a2a8fecd2d394fcd127
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2017-04-12 10:43:58 +0200

    gerard es gay

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
   $('select').material_select();
+  $('button').on('click', () => {
+    const a = $('select');
+    const calc = a[0].value * a[1].value * 1.5;
+    console.log(calc);
+  });
 });
-          
