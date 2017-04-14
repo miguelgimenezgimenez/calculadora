289cf3d30c90987b683257a4780f1de3ee1416c0
Miguel Gimenez <miguel.gimenez@utopiamusic.com>
2017-04-14 09:16:05 +0200

    gerrri gayy

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
+  $('#test1').prop('checked',true);
   $('select').material_select();
+  $('boat-type').on('click', (args) => {})
+
   $('button').on('click', () => {
-    const a = $('select');
-    const calc = a[0].value * a[1].value * 1.5;
-    console.log(calc);
+    var factor=1;
+    if ($('#test1')[0].checked) factor = 1.5;
+    var a = $('select');
+    var calc = a[0].value * a[1].value * factor;
+    $('.amount').html(calc+'$');
   });
 });
